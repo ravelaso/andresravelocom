88634c89cb620c732d7da77b92a7aac2bd920f39
Andres Ravelo <ravelaso@proton.me>
2025-08-06 09:31:40 +0200

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.50.8
      project name = andresravelocom
      framework = astro
      framework cli = create-astro@4.13.1
      package manager = npm@11.5.1
      wrangler = wrangler@4.28.0
      git = 2.50.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 import cloudflare from '@astrojs/cloudflare';
-
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
 
-  adapter: cloudflare(),
-  integrations: [react()]
+    imageService: "cloudflare"
+  })
 });
