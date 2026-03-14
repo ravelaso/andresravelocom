ceee8ec89ad1208afa294b8f91af6e9cbd34e9b7
andrecab <andres.caballero2@dhl.com>
2026-03-14 16:37:14 +0100

    output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: cloudflare({
     imageService: 'cloudflare-binding',
   }),
